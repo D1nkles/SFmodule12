4946c6f1dfe96630f1f8f915776763d64c28d9c0
Ilia <isinetov@mail.ru>
2024-05-10 21:01:10 +0400

    Задание 12.2.2. Исправьте излишнюю вложенность в коде:

---
+++ Task 12.2.2/Task 12.2.2.cs
@@ -0,0 +1,22 @@
+﻿static void GetPageForUser(User user)
+{
+    if (!user.LoggedIn)
+        return;
+
+    Console.WriteLine("User logged in");
+
+    if (!user.HasAccessToSection)
+        return;
+
+    Console.WriteLine("User has access to that section");
+
+    if (user.HasUnseenNews)
+    {
+        ShowNews();
+    }
+
+    if (user.HasAdminRights)
+    {
+        DecorateAdminTools();
+    }
+}
